73586f5d5020c37ccf18830dbce07a78843d9ef8
adrianopatrick <adrianopatrick@gmail.com>
2016-02-17 18:36:07 -0300

    ajustado script initsql

--- saa-core/resources/initsql.sql
+++ saa-core/resources/initsql.sql
@@ -6,7 +6,7 @@ insert into permissoes values (1, 'Cadastrar Aula', 'CADASTRAR_AULA', 1), (2, 'A
 	(3, 'Excluir Aula', 'EXCLUIR_AULA', 1);
 	
 --PAPEIS
-insert into papeis values (1, 0), (2,1);
+insert into papeis values (1, 0), (2,1), (3, 2);
 
 --CONFIGURACOES DE ATENDENTE
 insert into configuracoes values (1, 1), (1, 2), (1, 3);
@@ -15,4 +15,4 @@ insert into configuracoes values (1, 1), (1, 2), (1, 3);
 insert into usuarios values (1, true, 'admin', false, '3C9909AFEC25354D551DAE21590BB26E38D53F2173B8D3DC3EEE4C047E7AB1C1EB8B85103E3BE7BA613B31BB5C9C36214DC9F14A42FD7A2FDB84856BCA5C44C2');
 
 --PERFIS DE ADM
-insert into perfis values (1, 1), (1,2);
+insert into perfis values (1, 1), (1,2), (1, 3);
